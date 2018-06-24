50def5dcd933b77a4da30e5deafb25e89172e9b2
Sean Altemose <sean@seanaltemose.com>
2018-06-24 15:58:04 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,75 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = <<-SQL
+      pragma table_info('#{table_name}')
+    SQL
+
+    column_names = []
+    table_info = DB[:conn].execute(sql)
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if { |col| col == "id"  }.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
+    SQL
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name} WHERE name = '#{name}'
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attribute)
+    key = attribute.keys[0]
+    value = attribute.values[0]
+
+    sql = <<-SQL
+      SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'
+    SQL
+    DB[:conn].execute(sql)
+  end
+
 end
